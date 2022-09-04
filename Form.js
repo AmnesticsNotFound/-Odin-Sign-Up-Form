e7c7f1c572002adf863d5f7d9c0ad441ab3c7932
AmnesticsNotFound <50062894+AmnesticsNotFound@users.noreply.github.com>
2022-09-04 14:47:03 -0500

    Added Project
    Upload of files

---
+++ Form.js
@@ -0,0 +1,16 @@
+function checkpass () {
+var a = document.querySelector("#password");
+var b = document.querySelector("#passwordC");
+var c = document.querySelector(".hid");
+if (a.value == "" && b.value == ""){
+    c.style.value = "Passw"
+}
+if (a.value != b.value) {
+    
+    c.style.display = 'inline'
+}
+else {
+    c.style.display = 'none'
+}
+
+}
